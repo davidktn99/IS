e57f4ead9b709c9999c7f94d16c6a8b7b99d0479
davidktn99 <72510289+davidktn99@users.noreply.github.com>
2021-01-12 12:02:51 +0200

    cleanup

--- sql sources/006.sql
+++
@@ -1,7 +0,0 @@
-#Tutorial 006
-
-CREATE TABLE users3 (id INT NOT NULL, username TEXT NOT NULL);
-DESC users3;
-#ERROR INSERT INTO users3 (id, username) VALUES (NULL, NULL);
-#ERROR INSERT INTO users3 (id) VALUES (7);
-#ERROR INSERT INTO users3 (username) VALUES ("Billy"); 
--- sql sources/010.sql
+++
@@ -1,6 +0,0 @@
-#Tutorial 010
-
-CREATE TABLE users4(id INT PRIMARY KEY, username TEXT, email TEXT);
-INSERT INTO users4 (id, username, email) VALUES (1, "Rob", "jhfgd");
-INSERT INTO users4 (id, username, email) VALUES (2, "Bob", "hgtfsdhs");
-# ERROR INSERT INTO users4 (id, username, email) VALUES (2, "Lewis", "fsadef"); 
